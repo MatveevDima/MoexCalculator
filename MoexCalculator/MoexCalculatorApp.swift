4d3ce5bc770727c26a3adcfe81d18adc36c8f8c1
Дмитрий <monk-dee@mail.ru>
2024-09-26 11:20:41 +0300

    Initial Commit

---
+++ MoexCalculator/MoexCalculatorApp.swift
@@ -0,0 +1,17 @@
+//
+//  MoexCalculatorApp.swift
+//  MoexCalculator
+//
+//  Created by Александр Матвеев on 26.09.2024.
+//
+
+import SwiftUI
+
+@main
+struct MoexCalculatorApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
