27f321127b668374b6cfed738f5bb5870c4e1dae
Дмитрий <monk-dee@mail.ru>
2024-09-27 12:50:26 +0300

    first commit

--- MoexCalculator/ContentView.swift
+++ MoexCalculator/ContentView.swift
@@ -8,14 +8,23 @@
 import SwiftUI
 
 struct ContentView: View {
+    @State var color = Color.green                  // 1
+    
     var body: some View {
         VStack {
             Image(systemName: "globe")
                 .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+            Text("Привет, Практикум!")
+                .padding()
+                .foregroundColor(color)      // 2
+            Button("Нажми здесь!") {         // 3
+                if color == .green {
+                    color = .orange
+                } else {
+                    color = .green
+                }
+            }
         }
-        .padding()
     }
 }
 
--- MoexCalculator/MoexCalculatorApp.swift
+++ MoexCalculator/MoexCalculatorApp.swift
@@ -2,16 +2,19 @@
 //  MoexCalculatorApp.swift
 //  MoexCalculator
 //
-//  Created by Александр Матвеев on 26.09.2024.
+//  Created by Дмитрий Матвеев on 26.09.2024.
 //
 
 import SwiftUI
 
 @main
 struct MoexCalculatorApp: App {
-    var body: some Scene {
-        WindowGroup {
-            ContentView()
-        }
-    }
+    @StateObject var viewModel = CalculatorViewModel()
+     
+     var body: some Scene {
+         WindowGroup {
+             MainView()
+                 .environmentObject(viewModel)
+         }
+     }
 }
